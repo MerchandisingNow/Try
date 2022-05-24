e4ff11a74bec6568ab11a099f925bf1da2b4d59f
MerchandisingNow <106112300+MerchandisingNow@users.noreply.github.com>
2022-05-24 14:59:26 +0200

    Update script1.js

--- @free-Entrance_To001ALLazklmeeANK/script1.js
+++ @free-Entrance_To001ALLazklmeeANK/script1.js
@@ -230,7 +230,7 @@ function ajout_panier(){
 
     quantite = document.getElementById('qte').value;
     if((quantite == null)||(quantite == 0)) quantite = 1;
-    if(quantite < 0) quantite *=(-1);
+    if(quantite < 0) quantite = quantite * (-1);
     coul = document.getElementById('clr').value;
     prix = document.getElementById('up').innerHTML;
     nprod = document.getElementById('lib').innerHTML;
